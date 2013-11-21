ef7597d69b7038c446725343a13043958ac96b0e
Jennae Steinmiller <c15jennae.steinmiller@usafa.edu>
2013-11-21 15:07:49 -0700

    Updated timing module

--- RobotMovementLibrary.c
+++ RobotMovementLibrary.c
@@ -5,15 +5,19 @@
  *      Author: C15Jennae.Steinmiller
  */
 
-void motion(int direction) {
+void motion(int direction, int time) {
+	int i = 0;
 
-	TA0CCTL1 &= ~OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-	TA0CCTL0 &= ~OUTMOD_7;			//set to Reset
+	//Clears all of the pins
+	TA0CCTL1 &= ~OUTMOD_7;
+	TA0CCTL0 &= ~OUTMOD_7;
 	TA1CCTL1 &= ~OUTMOD_7;
 	TA1CCTL0 &= ~OUTMOD_7;
 
+	__delay_cycles(500000); //Allows for small break between different motions.
+
 	if (direction == 1) {
-		//to move forward - working!
+		//to move forward
 		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
 		TA0CCTL0 |= OUTMOD_5;			//set to Reset
 		TA1CCTL1 |= OUTMOD_7;
@@ -22,7 +26,7 @@ void motion(int direction) {
 	}
 
 	else if (direction == 2) {
-		//to move backward - working!
+		//to move backward
 		TA0CCTL1 |= OUTMOD_5;		//set to reset
 		TA0CCTL0 |= OUTMOD_4;		//set to toggle
 		TA1CCTL1 |= OUTMOD_5;		//set to reset
@@ -31,6 +35,7 @@ void motion(int direction) {
 	}
 
 	else if (direction == 3) {
+		//to move left
 		TA0CCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
 		TA0CCTL0 |= OUTMOD_5;			//set to Reset
 		TA1CCTL1 |= OUTMOD_5;
@@ -39,11 +44,16 @@ void motion(int direction) {
 
 	else {
 		//to move right
-		TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset / Set mode
-		TA0CCTL0 |= OUTMOD_7;			//set to Reset
+		TA0CCTL1 |= OUTMOD_5;        // set TACCTL1 to Reset
+		TA0CCTL0 |= OUTMOD_7;			//set to Reset / Set mode
 		TA1CCTL1 |= OUTMOD_7;
 		TA1CCTL0 |= OUTMOD_5;
 
+	}
+
+	while(i <= time){
+		i++;
+		__delay_cycles(1000);
 	}
 
 }
---
+++ RobotMovementLibrary.h
@@ -0,0 +1,13 @@
+/*
+ * RobotMovementLibrary.h
+ *
+ *  Created on: Nov 21, 2013
+ *      Author: C15Jennae.Steinmille
+ */
+
+#ifndef ROBOTMOVEMENTLIBRARY_H_
+#define ROBOTMOVEMENTLIBRARY_H_
+
+
+
+#endif /* ROBOTMOVEMENTLIBRARY_H_ */
